42895e74361a79587a5b03594e5a2e69b874120b
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-28 17:27:26 +0000

    can store value to already named hash, but need to amend this so user can define both key and hash

--- dbserver.rb
+++ dbserver.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
 
 class Dbserver < Sinatra::Base
+
+  enable :sessions
+
+
   get '/' do
     'Hello dbserver!'
+  end
+
+  get '/set' do
+  session[:cartoon] = params[:cartoon]
+  p session
+  "You've chosen " + params[:cartoon]
+
+  end
+
+  get '/get' do
+    "Here's you're saved cartoon: " + session[:cartoon]
   end
 
   # start the server if ruby file executed directly
--- spec/dbserver_spec.rb
+++ spec/dbserver_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 feature 'Dbserver' do
 
   scenario 'should be able to visit the home page' do
-      visit '/'
-      expect(page.status_code).to eq(200)
-    end
+    visit '/'
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'should have welcome note on home page' do
+    visit '/'
+    expect(page).to have_content('Hello dbserver!')
+  end
 
   scenario 'should be able to visit home page and port to be 4000' do
     visit '/'
@@ -13,3 +18,26 @@ feature 'Dbserver' do
   end
 
 end
+
+
+
+# scenario 'should have name of cartoon in the url' do
+#   visit '/set'
+#   session[:cartoon] =
+#
+#   expect(page.current_url).to eq('http://localhost:4000/')
+# end
+
+# describe 'The Dbserver App' do
+#   include Rack::Test::Methods
+#
+#   def app
+#     Sinatra::Application
+#   end
+#
+#   it "tests this rack milarky" do
+#     get '/'
+#     expect(last_response).to be_ok
+#     expect(last_response.body).to eq('Hello dbserver!')
+#   end
+# end
